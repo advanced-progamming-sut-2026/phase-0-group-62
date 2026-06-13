20637424bbe713384edc69e2f2cb9a8624dd490c
kian.Hj <kiyanhajipour1@gmail.com>
2026-06-13 11:46:57 +0330

    Edit ZombieLoader.java

---
+++ src/model/entities/zombie/loader/ZombieLoader.java
@@ -0,0 +1,12 @@
+package models.entities.zombies.loader;
+
+import java.util.List;
+
+public class ZombieLoader {
+    
+    // خوندن JSON و برگردوندن لیست داده‌ها
+    public List<ZombieData> loadZombies() {
+        // TODO: خوندن فایل resources/zombies.json
+        return null;
+    }
+}
