3c7af5f408f59e5d88b2f151a73540669c987a29
darrrth <a1l3i8r6@gmail.com>
2026-07-14 20:21:46 +0330

    Update PlantTag.java

--- src/model/enums/PlantTag.java
+++ src/model/enums/PlantTag.java
@@ -1,12 +1,31 @@
 package model.enums;
 
 public enum PlantTag {
+    // Base tags
     SHOOTER,
     SUN_PRODUCER,
     DEFENDER,
     EXPLOSIVE,
     INSTANT_ATTACK,
     SUPPORT,
-    AQUATIC
+    AQUATIC,
+    
+    // Additional tags
+    DAY,
+    NIGHT,
+    SHROOM,
+    GROWTH,
+    PEA,
+    ICE,
+    FIRE,
+    STACKABLE,
+    CHARGE,
+    MAGIC,
+    POISON,
+    WATER,
+    AOE,
+    TRAP,
+    MOVER,
+    SUN,
+    MINT
 }
-
