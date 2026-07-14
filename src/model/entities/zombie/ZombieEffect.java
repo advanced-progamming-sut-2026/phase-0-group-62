b9de7165dc79ded8cbf4545691d1c109df4ca35f
darrrth <a1l3i8r6@gmail.com>
2026-07-14 20:20:43 +0330

    Update ZombieEffect.java

--- src/model/entities/zombie/ZombieEffect.java
+++ src/model/entities/zombie/ZombieEffect.java
@@ -1,4 +1,22 @@
-package models.entities.zombies;
+package model.entities.zombie;
 
 public enum ZombieEffect {
+    NONE,
+    CHILLED,        // Slowed movement
+    FROZEN,         // Completely frozen
+    POISONED,       // Damage over time
+    HYPNOTIZED,     // Fighting for player
+    ON_FIRE,        // Burning
+    GLOWING,        // Drops plant food on death
+    ARMORED,        // Has armor
+    STUNNED,        // Temporarily stunned
+    SPINNING,       // Jester spinning
+    DIVING,         // Underwater
+    FLYING,         // Dodo riding
+    ENRAGED,        // Newspaper zombie enraged
+    BLESSED,        // Knight blessed by King
+    ROOTED,         // Stuck in place
+    SLOWED,         // Movement speed reduced
+    FAST,           // Movement speed increased
+    HASTE           // Attack speed increased
 }
